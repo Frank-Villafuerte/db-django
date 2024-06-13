1723c7dec5391df69222ee924ac92037b16635e4
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2024-06-13 01:07:26 -0500

    Completamos la implementación de la app que envía mensajes

--- proyecto/mails_envio/views.py
+++ proyecto/mails_envio/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 def index(request):
-    return render(request, mails_envio/index.html)
+    send_mail(
+    "Mensaje Django",
+    "El mensaje se ha enviado exitosamente.",
+    "prositofrank@gmail.com",
+    ["fvillafuerte@unsa.edu.pe"],
+    fail_silently=False,
+)
+    return render(request, 'mails_envio/index.html')
