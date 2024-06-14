3d51a3667abb2927c8648363b346761958973071
Frank Villafuerte <133295276+Frank-Villafuerte@users.noreply.github.com>
2024-06-14 16:44:23 -0500

    Cambiado settings.py para que no muestre la contraseña

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -128,6 +128,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_HOST_USER= 'prositoelmo@gmail.com'
-EMAIL_HOST_PASSWORD = 'eqdw dfyh fvjv ymnu'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS= True
 EMAIL_USE_SSL = False
