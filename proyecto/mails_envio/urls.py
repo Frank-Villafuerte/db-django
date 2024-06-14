216e585c0a2f104321fa03bb0400c08ff17a17e8
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2024-06-13 23:35:36 -0500

    Implementamos un índice para acceder a cada aplicación

--- proyecto/mails_envio/urls.py
+++ proyecto/mails_envio/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
+    path('enviar_mail', views.email,name='enviar_mail'),
+    path('pdf', views.pdf_view,name='pdf'),
+    #path('', views.index,name='index'),
+    #path('', views.index,name='index'),
 ]
--- proyecto/mails_envio/views.py
+++ proyecto/mails_envio/views.py
@@ -1,11 +1,29 @@
+import datetime
 from django.shortcuts import render
 from django.core.mail import send_mail
+
+from django.http import Http404
+
+from . import renderers
 def index(request):
+    return render(request, 'mails_envio/index.html')
+
+def email(request):
     send_mail(
     "Mensaje Django",
     "El mensaje se ha enviado exitosamente.",
     "prositofrank@gmail.com",
     ["fvillafuerte@unsa.edu.pe"],
     fail_silently=False,
-)
-    return render(request, 'mails_envio/index.html')
+    )
+    return render(request, 'mails_envio/enviar_mail.html')
+
+
+def pdf_view(request, *args, **kwargs):
+    data = {
+        'date': datetime.date.today(), 
+        'amount': 39.99,
+        'customer_name': 'Cooper Mann',
+        'invoice_number': 1233434,
+    }
+    return renderers.render_to_pdf('mails_envio/invoice.html', data)
